c7e3e0eebe9c4c7f85f47990c8f640468f09aa7a
sayoshigure11 <shun255105@outlook.jp>
2025-12-03 21:14:04 +0900

    manifest.tsの中身を少し書き換えた

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,30 +2,54 @@ import type { MetadataRoute } from 'next'
 
 export default function manifest(): MetadataRoute.Manifest {
     return {
-        "theme_color": "#8936FF",
-        "background_color": "#2EC6FE",
-        "icons": [
+        name: 'BookArchive',
+        short_name: 'BookArchive',
+        description: '本棚の管理をするアプリです。',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#2EC6FE',
+            theme_color: '#8936FF',
+        lang: "ja",
+        icons: [
             {
-                "purpose": "maskable",
-                "sizes": "512x512",
-                "src": "icon512_maskable.png",
-                "type": "image/png"
+                purpose: 'maskable',
+                sizes: '512x512',
+                src: '/icon512_maskable.png',
+                type: 'image/png'
             },
             {
-                "purpose": "any",
-                "sizes": "512x512",
-                "src": "icon512_rounded.png",
-                "type": "image/png"
-            }
+                purpose: 'any',
+                src: '/icon512_rounded.png',
+                sizes: '512x512',
+                type: 'image/png',
+            },
         ],
-        "orientation": "any",
-        "display": "standalone",
-        "dir": "auto",
-        "lang": "ja",
-        "name": "BookArchive",
-        "short_name": "BookArchive",
-        "start_url": "https://book-archive-xi.vercel.app/",
-        "scope": "https://book-archive-xi.vercel.app/",
-        "description": "本棚の管理をするアプリです。"
     }
+    // {
+    //     "theme_color": "#8936FF",
+    //     "background_color": "#2EC6FE",
+    //     "icons": [
+    //         {
+    //             "purpose": "maskable",
+    //             "sizes": "512x512",
+    //             "src": "icon512_maskable.png",
+    //             "type": "image/png"
+    //         },
+    //         {
+    //             "purpose": "any",
+    //             "sizes": "512x512",
+    //             "src": "icon512_rounded.png",
+    //             "type": "image/png"
+    //         }
+    //     ],
+    //     "orientation": "any",
+    //     "display": "standalone",
+    //     "dir": "auto",
+    //     "lang": "ja",
+    //     "name": "BookArchive",
+    //     "short_name": "BookArchive",
+    //     "start_url": "https://book-archive-xi.vercel.app/",
+    //     "scope": "https://book-archive-xi.vercel.app/",
+    //     "description": "本棚の管理をするアプリです。"
+    // }
 }
